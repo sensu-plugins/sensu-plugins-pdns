6006bf0235890f6276678ffd5d59b9b40fd79b39
Eric Heydrick <eheydrick@gmail.com>
2017-08-27 20:29:13 -0700

    simplify the check and make the naming scheme configurable (#3)
    * simplify the check and make the naming scheme configurable
    
    * add a test that will pass

--- bin/metrics-pdns.rb
+++ bin/metrics-pdns.rb
@@ -1,10 +1,9 @@
-#! /usr/bin/env ruby
+#!/usr/bin/env ruby
 #
-# <metrics-pdns.rb>
+# metrics-pdns.rb
 #
 # DESCRIPTION:
-# simple ruby script to extract PowerDNS recursor statistics and send them in Graphite PlainText format
-
+#   Extract PowerDNS recursor statistics and send them in Graphite plaintext format
 #
 # OUTPUT:
 #   plain text metric data
@@ -16,8 +15,10 @@
 #   gem: sensu-plugin
 #
 # USAGE:
+#   metrics-pdns.rb
 #
 # NOTES:
+#   Requires sudo permissions for the sensu user to run rec_control
 #
 # LICENSE:
 #   <Hammad Shah>  <haashah@gmail.com>
@@ -28,36 +29,23 @@
 require 'sensu-plugin'
 require 'sensu-plugin/metric/cli'
 require 'English'
+require 'socket'
 
 class PdnsGraphite < Sensu::Plugin::Metric::CLI::Graphite
-  option :dump_file,
-         description: 'name of file to dump powerDNS stats to',
-         short: '-d DUMPFILE',
-         long: '--dump DUMPFILE',
-         default: 'tmp.txt'
-
-  def output(*args)
-    return unless args.empty?
-    args[2] = Time.now.to_is if args[2].nil?
-    puts "#{`hostname -f`.chomp}-- #{args[0..2].join('\s')}"
-  end
+  option :scheme,
+         description: 'Metric naming scheme, text to prepend to metric',
+         long: '--scheme SCHEME',
+         default: "#{Socket.gethostname}.pdns"
 
   def run
-    # dump powerdns statistics using rec control
-    # TODO: @haashah only extract user specified metrics.
-    system "sudo rec_control get-all > /tmp/#{config[:dump_file]}"
+    metrics = `sudo rec_control get-all`.split("\n")
     if $CHILD_STATUS.exitstatus > 0
-      puts 'failed to dump pdns statistics. check pdns process is running!'
-      exit 2
+      critical 'Failed to dump pdns statistics. Check that the pdns process is running!'
     end
-    File.readlines("/tmp/#{config[:dump_file]}").each do |line|
-      output line.split[0], line.split[1]
+    metrics.each do |metric|
+      (key, value) = metric.split("\t")
+      output([config[:scheme], key].join('.'), value)
     end
-  end
-
-  def teardown
-    # time to cleanup dump file
-    system "sudo rm -rf /tmp/#{config[:dump_file]}"
-    exit 2 if $CHILD_STATUS.exitstatus > 0
+    ok
   end
 end
